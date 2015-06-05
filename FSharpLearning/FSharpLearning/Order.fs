43dab5f62ebd592b777b2be224c82cc81288b53a
Colin Annetti <cbannetti@gmail.com>
2015-06-04 20:48:37 -0500

    Added Order class

---
+++ FSharpLearning/FSharpLearning/Order.fs
@@ -0,0 +1,16 @@
+﻿module Order
+
+type OrderSide =
+    Buy | Sell
+
+type OrderType =
+    Limit | Market
+
+type Order(Security : string, Side : OrderSide, Type : OrderType, Qty : int, LimitPrice : decimal, CounterpartyName : string)   =
+    let security = Security
+    let side = Side
+    let orderType = Type
+    let quantity = Qty
+    let limitprice = LimitPrice
+    let counterpartyName = CounterpartyName
+
