31cc3513bdcd7ce3ddc55ed52536853986ae8798
Colin Annetti <cbannetti@gmail.com>
2015-06-11 20:54:20 -0500

    added modeling for ask book and bid book

--- FSharpLearning/FSharpLearning/Security.fs
+++ FSharpLearning/FSharpLearning/Security.fs
@@ -1,2 +1,14 @@
 ﻿module Security
+open Order
 
+type AskBook(orders : List<Order>)=
+    member this.Orders = orders
+    member this.Add (order: Order) : AskBook =
+    member this.Process(order: Order) : (Some<Order>, AskBook) =
+type BidBook(orders : List<Order>) =
+    member this.Orders = orders
+    member this.Add (order : Order) : BidBook =
+    member this.Process(order : Order) : (Some<Order>), BidBook) =
+
+type Security =
+    
