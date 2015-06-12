fc25b994f31b5da6e648532375f52413a4f7e3ad
Colin Annetti <cbannetti@gmail.com>
2015-06-11 20:52:34 -0500

    added modeling for askbook and bidbook

--- FSharpLearning/FSharpLearning/Order.fs
+++ FSharpLearning/FSharpLearning/Order.fs
@@ -6,10 +6,10 @@ type OrderSide =
 type OrderType =
     Limit | Market
 
-type Order(Security : string, Side : OrderSide, Type : OrderType, Qty : int, LimitPrice : decimal, CounterpartyName : string)   =
+type Order(Security : string, Side : OrderSide, Type : OrderType, Qty : int, LimitPrice : decimal, CounterPartyName : string)   =
     member this.security = Security
     member this.side = Side
     member this.orderType = Type
     member this.quantity = Qty
     member this.limitPrice = LimitPrice
-    member this.counterPartyName = CounterpartyName 
+    member this.counterPartyName = CounterPartyName 
