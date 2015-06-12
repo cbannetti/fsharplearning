8d80d9d302f24c7c90ce4a88870f1a8de5049b56
Colin Annetti <cbannetti@gmail.com>
2015-06-11 21:04:37 -0500

    changed the modelling of the orderbooks so that types did not have functions

--- FSharpLearning/FSharpLearning/Order.fs
+++ FSharpLearning/FSharpLearning/Order.fs
@@ -6,7 +6,7 @@ type OrderSide =
 type OrderType =
     Limit | Market
 
-type Order(Security : string, Side : OrderSide, Type : OrderType, Qty : int, LimitPrice : decimal, CounterPartyName : string)   =
+type Order(Security : string, Side : OrderSide, Type : OrderType, Qty : int, LimitPrice : decimal, CounterPartyName : string) =
     member this.security = Security
     member this.side = Side
     member this.orderType = Type
--- FSharpLearning/FSharpLearning/Security.fs
+++ FSharpLearning/FSharpLearning/Security.fs
@@ -1,14 +1,15 @@
 ﻿module Security
 open Order
 
+
 type AskBook(orders : List<Order>)=
     member this.Orders = orders
-    member this.Add (order: Order) : AskBook =
-    member this.Process(order: Order) : (Some<Order>, AskBook) =
 type BidBook(orders : List<Order>) =
     member this.Orders = orders
-    member this.Add (order : Order) : BidBook =
-    member this.Process(order : Order) : (Some<Order>), BidBook) =
+ 
+let askAdd (order: Order) : AskBook =
+let askProcess(order: Order) : (Some<Order>, AskBook) =
+let bidAdd (order : Order) : BidBook =
+let bidProcess(order : Order) : (Some<Order>), BidBook) =
 
 type Security =
-    
